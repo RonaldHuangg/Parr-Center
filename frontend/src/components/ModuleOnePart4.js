620c2abe93d955fb8c9282d7c508055dbea2eaca
Lucy Good <lucykgood@gmail.com>
2025-04-21 03:50:16 +0000

    Fix rendering issue

--- frontend/src/components/ModuleOnePart4.js
+++ frontend/src/components/ModuleOnePart4.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useState } from "react";
 import './ModuleOnePart4.css';
 import NavbarModule from "./Navbar_module";
